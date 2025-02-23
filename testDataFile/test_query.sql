a4f0685643f2eb743a554bdc1e996089c4f6cee3
ymatushevych-appwell <ymatushevych-appwell@ancestry.com>
2025-02-23 09:14:36 -0800

    chore[server]: added gitignore, testquery data

---
+++ testDataFile/test_query.sql
@@ -0,0 +1,6 @@
+-- Sample SQL Query for Testing File Upload
+SELECT name, database, engine FROM system.tables ORDER BY name LIMIT 10;
+
+-- Additional Queries for Testing
+SELECT now(); -- Get current server time
+SELECT count(*) FROM system.tables; -- Count total tables in system
